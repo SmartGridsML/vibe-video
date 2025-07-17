da9be88a6904de16600fc89a70cf847b0f3d0bf2
Bilal <bilal.saleem_96@outlook.com>
2025-07-17 16:22:47 +0100

    project files

---
+++ vibe-videoTests/vibe_videoTests.swift
@@ -0,0 +1,17 @@
+//
+//  vibe_videoTests.swift
+//  vibe-videoTests
+//
+//  Created by Saleem, Bilal on 17/07/2025.
+//
+
+import Testing
+@testable import vibe_video
+
+struct vibe_videoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ vv/vibe_videoApp.swift
@@ -0,0 +1,17 @@
+//
+//  vibe_videoApp.swift
+//  vibe-video
+//
+//  Created by Saleem, Bilal on 17/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct vibe_videoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
